df8f110c38cb1f40f1f89b5e1d99fc41a8d1002a
IT310Jomar <occ.otoc.jomar01@gmail.com>
2025-05-01 18:08:32 +0800

    test

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -1,5 +1,5 @@
 // import AppLogoIcon from '@/components/app-logo-icon';
-import JomarLogoIcon from '@/components/jdo-log';
+// import JomarLogoIcon from '@/components/jdo-log';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
@@ -21,7 +21,7 @@ export default function AuthSimpleLayout({ children, title, description }: Props
                     <div className="flex flex-col items-center gap-4">
                         <Link href={route('home')} className="flex flex-col items-center gap-2 font-medium">
                             <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                            <JomarLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white"/>
+                            {/* <JomarLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white"/> */}
                             </div>
                             <span className="sr-only">{title}</span>
                         </Link>
--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -9,10 +9,7 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
+
 
 
 type LoginForm = {
